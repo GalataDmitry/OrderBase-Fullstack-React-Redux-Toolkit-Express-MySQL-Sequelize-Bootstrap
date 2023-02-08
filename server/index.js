d1712de61050ae8087d519efdba6db7315fdab25
Uncle Niko <uncleniko@uncleniko.com>
2023-02-08 20:46:32 +0200

    updeted server index

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = 5000
 app.use(cors({
     // origin: '*'
-    origin: 'http://192.168.1.102:3040'
+    // origin: 'http://192.168.1.102:3040'
     // origin: 'http://192.168.0.5:3000'
 }))
 app.use(express.json())
