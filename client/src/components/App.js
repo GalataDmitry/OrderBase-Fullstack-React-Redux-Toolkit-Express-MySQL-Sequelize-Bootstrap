e30d4159396094e049ba0b0a178df851c14a6979
Uncle Niko <uncleniko@uncleniko.com>
2023-02-20 13:32:47 +0200

    cicd test success#3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -28,7 +28,7 @@ function App() {
                 </> :
                 allClients?.map(client => {
                     return <div className='container-fluid w-50'>
-                        Test!!
+                        Test!!333
                         <hr/>
                         <div className='row'>
                             <div className='col'>
